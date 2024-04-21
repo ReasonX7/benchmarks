a4672128d15abc9269fd2b012b60f5f065539337
ReasonX7 <reasonx7@gmail.com>
2024-04-21 22:41:35 +0300

    feat(js): loops

---
+++ js/loops/index.mjs
@@ -0,0 +1,49 @@
+import { Bench } from 'tinybench';
+
+const bench = new Bench({
+  time: 1,
+  iterations: 1,
+  warmupTime: 1000,
+  warmupIterations: 100,
+});
+
+const nums = Array.from({ length: 1_000_000 }).map((_, index) => index);
+
+const forClassic = () => {
+  let sum = 0;
+  for (let index = 0; index < nums.length; index++) {
+    sum += nums[index];
+  }
+  return sum;
+};
+
+const forOf = () => {
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+  }
+  return sum;
+};
+
+const forEach = () => {
+  let sum = 0;
+  nums.forEach((value) => {
+    sum += value;
+  });
+  return sum;
+};
+
+const forReduce = () => {
+  return nums.reduce((sum, num) => sum + num);
+};
+
+bench
+  .add('For Classic', forClassic)
+  .add('For Of', forOf)
+  .add('For Each', forEach)
+  .add('For Reduce', forReduce);
+
+// await bench.warmup();
+await bench.run();
+
+console.table(bench.table());
